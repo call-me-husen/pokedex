c9227548f3e4f9e4c5c8c58313bb696d0b61a93e
call-me-husen <muhnrndhsn@gmail.com>
2023-09-03 03:04:54 +0700

    refactor: add vercel url

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,4 +1,4 @@
 const POKE_API_URL = process.env.NEXT_PUBLIC_POKE_API_URL;
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL;
 
 export { POKE_API_URL, BASE_URL };
